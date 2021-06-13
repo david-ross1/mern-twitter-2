3b3ec6447729a02c6f5f19dfc505135a226c20bb
dross <davidr7711@gmail.com>
2021-06-13 08:51:38 -0700

    Complete user model, registration, bcrypt

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-const mongoose = require('mongoose'); 
+const mongoose = require("mongoose");
 const express = require("express");
-
 const app = express();
-const db = require('./config/keys').mongoURI; 
+const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const User = require("./models/User");
+const bodyParser = require("body-parser");
 
 mongoose
-  .connect(db,  {useNewUrlParser: true, useUnifiedTopology: true})
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("hello world"));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
+app.use(bodyParser.json());
+
+
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
