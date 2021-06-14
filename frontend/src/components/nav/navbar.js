b1ff55dc16487db11bcc6591f50716c4ae76a8fa
dross <davidr7711@gmail.com>
2021-06-13 20:57:17 -0700

    Success storing users

---
+++ frontend/src/components/nav/navbar.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import './navbar.css'
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.logoutUser = this.logoutUser.bind(this);
+    this.getLinks = this.getLinks.bind(this);
+  }
+
+  logoutUser(e) {
+      e.preventDefault();
+      this.props.logout();
+  }
+
+  getLinks() {
+      if (this.props.loggedIn) {
+        return (
+          <div className="NavBar">
+            <div className="Links2">
+                <Link to={'/tweets'}>All Tweets</Link>
+                <Link to={'/profile'}>Profile</Link>
+                <Link to={'/new_tweet'}>Write a Tweet</Link>
+                <button onClick={this.logoutUser}>Logout</button>
+            </div>
+            </div>
+        );
+      } else {
+        return (
+            <div className="NavBar Links2">
+                <Link to={'/signup'}>Signup</Link>
+                <Link to={'/login'}>Login</Link>
+            </div>
+        );
+      }
+  }
+
+  render() {
+      return (
+        <div className='Links2'>
+            <h1>Chirper</h1>
+            { this.getLinks() }
+        </div>
+      );
+  }
+}
+
+export default NavBar;
